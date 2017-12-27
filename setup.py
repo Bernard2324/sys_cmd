dd26ee589f44dd27fe88d488f3637f047a96d516
Maurice <bernard.infosystems@gmail.com>
2017-12-27 17:39:58 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name='sysware_cmd',
+	version='1.1.1',
+	install_requires = [
+		"requests>=1.0"
+	],
+	description="Command Line Tool For SysAdmins",
+	author="Maurice Green",
+	author_email="bernard.infosystems@gmail.com",
+	license="MIT License: Copyright (c) 2017 Maurice Green",
+	keywords="linux zabbix vmware python command-line-tool",
+	url='http://github.com/Bernard2324/sysware_cmd',
+	classifiers=[
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 2.7",
+		"License :: OSI Approved :: MIT",
+		"Operating System :: Linux",
+		"Development Status :: 1 - Beta",
+		"Natural Language :: English",
+		"Topic :: System :: Systems Administration"
+	],
+	packages=['sysware_cmd', 'sysware_cmd.connections', 'sysware_cmd.vmware', 'sysware_cmd.shell', 'sysware_cmd.zabbix', 'sysware_cmd.connections.libraryapi', 'sysware_cmd.connections.Exceptions']
+)
+
